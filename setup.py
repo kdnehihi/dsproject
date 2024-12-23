86649a2827ec693a0c26df21a172c2212c49bb1e
Khoa Tran <dinhdangkhoatran2005@gmail.com>
2024-12-22 20:14:08 -0600

    second commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    input: file name (text file)
+    output: list of requirements in text file
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n", " ") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+setup(
+    name='dsproject',
+    version= '0.0.1',
+    author='Khoa Tran',
+    author_email= 'dinhdangkhoatran2005@gmail.com',
+    install_requires= get_requirements('requirements.txt'),
+    packages= find_packages(),
+)
